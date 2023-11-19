a20ba1224bbfee86db62a46efaeaa0623e16c715
abdul <abdul2213@gmail.com>
2023-11-19 16:11:21 +0000

    Final Cucumber tes for Calculator

---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,17 @@
+public class MyStepdefs {
+   // @io.cucumber.java.en.Given("^the numbers (\\d+) and (\\d+)$")
+    public void theNumbersAnd(int arg0, int arg1) {
+    }
+
+   // @io.cucumber.java.en.When("^I add the two numbers$")
+    public void iAddTheTwoNumbers() {
+    }
+
+  //  @io.cucumber.java.en.Then("^the result should be (\\d+)$")
+    public void theResultShouldBe(int arg0) {
+    }
+
+ //   @io.cucumber.java.en.When("^I subtract the two numbers$")
+    public void iSubtractTheTwoNumbers() {
+    }
+}
